f5eb1a75c630e5d79351c7976c4da011c06cd814
Zack <bhmath@gmail.com>
2012-08-21 06:55:34 -0700

    Minor revert of boolean converter.

---
+++ trunk/Launcher/Common/BooleanVisibilityConverter.cs
@@ -0,0 +1,42 @@
+﻿namespace Launcher.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Data;
+    using System.Windows;
+
+    public class BooleanVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is Boolean))
+            {
+                throw new ArgumentException("", "value");
+            }
+
+            return ((bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is Visibility))
+            {
+                throw new ArgumentException("", "value");
+            }
+
+            switch (((Visibility)value))
+            {
+                case Visibility.Visible:
+                    {
+                        return true;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+    }
+}
---
+++ trunk/Launcher/Common/BooleanWindowStateConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace Launcher.Common
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class BooleanWindowStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is Boolean))
+            {
+                throw new ArgumentException("", "value");
+            }
+
+            return ((bool)value) ? WindowState.Normal : WindowState.Minimized;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is WindowState))
+            {
+                throw new ArgumentException("", "value");
+            }
+
+            switch (((WindowState)value))
+            {
+                case WindowState.Maximized:
+                case WindowState.Normal:
+                    {
+                        return true;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+    }
+}
