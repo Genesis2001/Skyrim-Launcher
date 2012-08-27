ebcf06d6a62af3499ff0dc29726ff3b10f36a55b
Zack <bhmath@gmail.com>
2012-08-27 16:51:32 -0700

    Multigame Support
    - Began adding multigame support.

---
+++ trunk/Launcher/Common/Modules/GameModuleBase.cs
@@ -0,0 +1,30 @@
+﻿namespace Launcher.Common.Modules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class GameModuleBase : IGameModule
+    {
+
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/Launcher/Common/Modules/IGameModule.cs
@@ -0,0 +1,28 @@
+﻿namespace Launcher.Common.Modules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+
+    public interface IGameModule : INotifyPropertyChanged
+    {
+        #region Properties
+
+        string Game { get; }
+
+        string DataPath { get; }
+        string GamePath { get; }
+        string InstallPath { get; }
+        string SavesLocation { get; }
+
+        #endregion
+
+        #region Methods
+
+        bool LoadCharacters();
+
+        #endregion
+    }
+}
